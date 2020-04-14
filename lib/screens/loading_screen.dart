c193f5a74456b6f7458961230ff313f2162ac463
anant jakhmola <anantjakhmola9@mail.com>
2020-04-14 15:07:19 +0530

    small wallpaper change

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -24,11 +24,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    //latitude = location.latitude;
-    //longitude = location.longitude;
+    latitude = location.latitude;
+    longitude = location.longitude;
 
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
 
     var weatherData = await networkHelper.getData();
     Navigator.push(context, MaterialPageRoute(builder: (context) {
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -40,7 +40,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
+            image: AssetImage('images/weather.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
